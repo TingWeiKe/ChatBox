5e0ca5a39b2f493f3094afef6100bd380b6b4f81
TingWeiKe <rockmanx673@gmail.com>
2019-03-27 16:27:36 +0800

    Implement django corsheader, set up React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import Axios from 'axios'
+import Linkify from 'react-linkify'
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+	constructor(props) {
+		super(props)
+		this.state = {
+			input: '',
+			result: 'Result',
+		}
   }
+  
+  getAnswer(){
+    Axios({
+			method: 'post',
+			url: 'http://127.0.0.1:8000/api/',
+			data: {
+        text: this.state.input,
+				user_id: 'rockmanx',
+			},
+		}).then((data) => {
+      console.log(data)
+      this.setState({ result: data.data.result })
+		})
+  }
+
+	hanldeInput(e) {
+		this.setState({ input: e.target.value })
+		console.log(this.state.input)
+	}
+
+	handleSubmit() {
+		this.getAnswer()
+	}
+
+	handleKeyDown(e) {
+		if (e.keyCode === 13) {
+      this.getAnswer()
+		}
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<input type='text' onKeyDown={(e) => this.handleKeyDown(e)} onChange={(e) => this.hanldeInput(e)} />
+				<div>
+					<button onClick={() => this.handleSubmit()}>Submit</button>
+					<Linkify>
+						<h3>{this.state.result}</h3>
+					</Linkify>
+				</div>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
