bba58e10f816b7680062b7279a70dbd415991be3
TingWeiKe <rockmanx673@gmail.com>
2019-03-27 15:29:05 +0800

    Commit before npm run ejecy

--- ChatBox/settings.py
+++ ChatBox/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
      #my Apps
     'rest_framework',
+    'corsheaders',
     'dialogue'
 
 ]
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
