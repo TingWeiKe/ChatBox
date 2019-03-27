c94bd20665c13121b4d6ba511df5f829001470ef
TingWeiKe <rockmanx673@gmail.com>
2019-03-27 21:07:09 +0800

    Refactoring in React Hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,36 @@
-import React, { Component } from 'react'
-import Axios from 'axios'
+import React from 'react'
+import axios from 'axios'
 import Linkify from 'react-linkify'
-class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			input: '',
-			result: 'Result',
-		}
-  }
-  
-  getAnswer(){
-    Axios({
-			method: 'post',
-			url: 'http://127.0.0.1:8000/api/',
-			data: {
-        text: this.state.input,
-				user_id: 'rockmanx',
-			},
-		}).then((data) => {
-      console.log(data)
-      this.setState({ result: data.data.result })
-		})
-  }
+import { useState, useEffect } from 'react'
 
-	hanldeInput(e) {
-		this.setState({ input: e.target.value })
-		console.log(this.state.input)
-	}
+export default function App(){
+	const [ result, setResult ] = useState('result')
+	const [ text, setText ] = useState(null)
 
-	handleSubmit() {
-		this.getAnswer()
-	}
-
-	handleKeyDown(e) {
+	function handleKeyDown(e){
 		if (e.keyCode === 13) {
-      this.getAnswer()
+			getSnswer(text)
 		}
 	}
 
-	render() {
-		return (
-			<div className='App'>
-				<input type='text' onKeyDown={(e) => this.handleKeyDown(e)} onChange={(e) => this.hanldeInput(e)} />
-				<div>
-					<button onClick={() => this.handleSubmit()}>Submit</button>
-					<Linkify>
-						<h3>{this.state.result}</h3>
-					</Linkify>
-				</div>
-			</div>
-		)
+	async function getSnswer(text){
+		if (text) {
+			const res = await axios.post('http://127.0.0.1:8000/api/', { text: text })
+			setResult(res.data.result)
+			console.log(res)
+		}
 	}
+
+	return (
+		<div className='App'>
+			<input type='text' onKeyDown={(e) => handleKeyDown(e)} onChange={(e) => setText(e.target.value)} />
+			<div>
+				<button onClick={() => getSnswer(text)}>Submit</button>
+				<Linkify>
+					<h3>{result}</h3>
+				</Linkify>
+			</div>
+		</div>
+	)
 }
 
-export default App
