4422d56cd39afc74f49cc318c77769093eaeaebb
TingWeiKe <rockmanx673@gmail.com>
2019-04-03 11:58:31 +0800

    Fix mobile RWD layout and fix git alert

--- frontend/src/SideList.js
+++ frontend/src/SideList.js
@@ -3,28 +3,9 @@ import React from 'react'
 export default function SideList(){
 	return (
 		<div className='sidelist'>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
-			<h1>sidebar</h1>
+			<h1>Header</h1>
+			<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+			<div className='footer'>Footer</div>
 		</div>
 	)
 }
